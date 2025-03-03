687e3f4fc4d53f4ae68ed9970b97b9485d090125
zabavb <bilonizkavik@gmail.com>
2025-03-03 16:53:07 +0100

    Implemented DbContext with configuartion file for Users and seeder, that are needed for MS SQL database interaction through EF Core

---
+++ UserAPI/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using UserAPI.Models;
+
+namespace UserAPI.Data.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(u => u.UserId)
+                .HasDefaultValueSql("NEWSEQUENTIALID()");
+
+            builder.Property(u => u.Username)
+                .IsRequired()
+                .HasMaxLength(20)
+                .HasColumnType("nvarchar(20)");
+
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("nvarchar(100)");
+
+            builder.HasIndex(u => u.Email)
+                .IsUnique();
+
+            builder.Property(u => u.PasswordHash)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("nvarchar(100)");
+        }
+    }
+}
---
+++ UserAPI/Data/DataSeeder.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserAPI.Models;
+
+namespace UserAPI.Data
+{
+    public class DataSeeder
+    {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+
+            var salt1 = PasswordHelper.GenerateSalt();
+            var hash1 = PasswordHelper.HashPassword("123456", salt1);
+            var user1 = new User
+            {
+                UserId = Guid.NewGuid(),
+                Username = "john.doe",
+                Email = "john.doe@example.com",
+                PasswordHash = hash1,
+                Salt = salt1
+            };
+
+            var salt2 = PasswordHelper.GenerateSalt();
+            var hash2 = PasswordHelper.HashPassword("654321", salt2);
+            var user2 = new User
+            {
+                UserId = Guid.NewGuid(),
+                Username = "jane.smith",
+                Email = "jane.smith@example.com",
+                PasswordHash = hash2,
+                Salt = salt2
+            };
+
+            modelBuilder.Entity<User>().HasData(user1, user2);
+        }
+    }
+}
