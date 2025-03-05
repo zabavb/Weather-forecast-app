8ccd249e1c1961ad05c92071650492243b87fd62
zabavb <bilonizkavik@gmail.com>
2025-03-05 08:44:00 +0100

    Implemented interface for sending request to remote weather API

---
+++ ForecastAPI/Services/IForecastApi.cs
@@ -0,0 +1,11 @@
+﻿using ForecastAPI.Models;
+using Refit;
+
+namespace ForecastAPI.Services
+{
+    public interface IForecastApi
+    {
+        [Get("/v1/forecast.json?key={apiKey}&q={location}&days={days}&aqi=no&alerts=no")]
+        Task<ForecastResponse> GetForecastAsync(string apiKey, string location, int days);
+    }
+}
