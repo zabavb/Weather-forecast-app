b240d79726aa3d0d5ebb1f8ddf2002234550e3a1
zabavb <bilonizkavik@gmail.com>
2025-03-05 08:40:49 +0100

    Implemented endpoint for retrievering data from remote weather API with xml documentation

--- ForecastAPI/Program.cs
+++ ForecastAPI/Program.cs
@@ -3,14 +3,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ForecastAPI",
+        Version = "v1"
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine($"{AppContext.BaseDirectory}", xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment()) 
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "ForecastAPI");
+    });
 }
 
 app.UseHttpsRedirection();
