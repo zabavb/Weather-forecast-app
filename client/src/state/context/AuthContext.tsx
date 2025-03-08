265d66a08607d3ea3a575f3738a12c0c694887a1
zabavb <bilonizkavik@gmail.com>
2025-03-08 15:30:13 +0100

    Implemented AuthContext for handling JWT authentication

---
+++ client/src/state/context/AuthContext.tsx
@@ -0,0 +1,65 @@
+import { createContext, SetStateAction, useContext, useState } from "react";
+import { loginService, registerService } from "../../services/authService";
+import { Login, Register, NotificationData, User } from "../../types";
+
+interface AuthContextProps {
+  user: User | null;
+  token: string | null;
+  login: (data: Login) => Promise<NotificationData>;
+  register: (data: Register) => Promise<NotificationData>;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [token, setToken] = useState<string | null>(localStorage.getItem("token"));
+
+  const login = async (userData: Login): Promise<NotificationData> => {
+    try {
+      const data = await loginService(userData);
+      authenticate(data);
+      return { type: "success", message: "Login successful!" };
+    } catch (error) {
+      console.error("Login failed:", error);
+      return { type: "error", message: "Invalid credentials. Please try again." }
+    }
+  };
+
+  const register = async (userData: Register): Promise<NotificationData> => {
+    try {
+      const data = await registerService(userData);
+      authenticate(data);
+      return { type: "success", message: "Register successful!" };
+    } catch (error) {
+      console.error("Register failed:", error);
+      return { type: "error", message: "Invalid credentials. Please try again." }
+    }
+  };
+
+  const logout = async () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("token_expiry");
+    localStorage.removeItem("user");
+    setUser(null);
+    setToken(null);
+  };
+
+  const authenticate = async (data: { token: string; expiresIn: number; user: SetStateAction<User | null>; }) => {
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("token_expiry", String(Date.now() + data.expiresIn * 60 * 1000));
+    localStorage.setItem("user", JSON.stringify(data.user));
+
+    setToken(data.token);
+    setUser(data.user);
+  }
+
+  return <AuthContext.Provider value={{ user, token, login, register, logout }}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  return context;
+};
