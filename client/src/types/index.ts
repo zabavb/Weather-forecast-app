169f59e5754898dc3b115639c7087e8179be4024
zabavb <bilonizkavik@gmail.com>
2025-03-08 15:31:41 +0100

    Implemented neccesary for TypeScript developmet types

---
+++ client/src/types/index.ts
@@ -0,0 +1,13 @@
+//Auth types
+
+export type { Login } from "./Login"
+export type { Register } from "./Register"
+
+// User types
+
+export type { User } from "./User"
+
+// Other types
+
+export type { Notification } from "./Notification"
+export type { NotificationData } from "./Notification"
