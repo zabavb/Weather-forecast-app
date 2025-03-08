7637510def46419218f7ce524cae73d934b8e0fa
zabavb <bilonizkavik@gmail.com>
2025-03-08 15:26:57 +0100

    Implemented API routes for interaction with UserAPI

---
+++ client/src/api/index.ts
@@ -0,0 +1,7 @@
+import { API_ROUTES } from "./config/apiConfig"
+
+// ================= AUTHENTICATION =================
+
+export const LOGIN = API_ROUTES.AUTH.LOGIN
+export const REGISTER = API_ROUTES.AUTH.REGISTER
+export const GET_USER_BY_ID = API_ROUTES.AUTH.GET_BY_ID
