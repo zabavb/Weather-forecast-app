674cae1a88af196f38b175bdace1d8c8d2ecabf3
zabavb <bilonizkavik@gmail.com>
2025-03-09 16:33:21 +0100

    Refcatored api routes by sending requests to gateway instead of directly to each API

--- client/src/api/config/apiConfig.ts
+++ client/src/api/config/apiConfig.ts
@@ -1,9 +1,12 @@
-const GATEWAY = `https://localhost:7241/gateway`
+const GATEWAY = `https://localhost:7241/gateway`;
+const AUTH = `${GATEWAY}/auth`;
+const WEATHER = `${GATEWAY}/weather`;
 
 export const API_ROUTES = {
-	AUTH: {
-		GET_BY_ID: (id: string) => `${GATEWAY}/auth/${id}`,
-		LOGIN: `${GATEWAY}/auth/login`,
-		REGISTER: `${GATEWAY}/auth/register`,
-	},
-}
+  AUTH: {
+    GET_BY_ID: (id: string) => `${AUTH}/${id}`,
+    LOGIN: `${AUTH}/login`,
+    REGISTER: `${AUTH}/register`,
+  },
+  WEATHER: (location: string, days: number) => `${WEATHER}/${location}/${days}`,
+};
