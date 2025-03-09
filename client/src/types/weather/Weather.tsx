a1eee348bf6079233a1801b39c47baae419c99e9
zabavb <bilonizkavik@gmail.com>
2025-03-09 17:10:50 +0100

    Implemented "Weather" type for maintaining readability of hadnling response from weather API

---
+++ client/src/types/weather/Weather.tsx
@@ -0,0 +1,31 @@
+export interface Weather {
+  location: {
+    name: string;
+    region: string;
+    country: string;
+  };
+  current: {
+    temp_c: number;
+    temp_f: number;
+    condition: {
+      text: string;
+      icon: string;
+    };
+    wind_kph: number;
+    humidity: number;
+  };
+  forecast: {
+    forecastday: {
+      date: string;
+      day: {
+        maxtemp_c: number;
+        mintemp_c: number;
+        avgtemp_c: number;
+        condition: {
+          text: string;
+          icon: string;
+        };
+      };
+    }[];
+  };
+}
